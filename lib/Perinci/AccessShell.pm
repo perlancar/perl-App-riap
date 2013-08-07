76bc422cab7f145a392001700601581aef015f2d
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-08-07 18:28:15 +0700

    Initial commit (not yet implemented)

---
+++ lib/Perinci/AccessShell.pm
@@ -0,0 +1,30 @@
+package Perinci::Access;
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub new {
+}
+
+sub run {
+}
+
+1;
+# ABSTRACT: Implementation for peri-sh script
+
+=head1 SYNOPSIS
+
+Use the provided L<peri-sh> script.
+
+
+=head1 DESCRIPTION
+
+This is the backend/implementation for the L<peri-sh> script.
+
+
+=head1 SEE ALSO
+
+L<Perinci::Access>
+
+=cut
